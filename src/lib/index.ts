d7a0be855aca0c07c00d33c20161a17ae11412b2
larabng <banlar01@students.zhaw.ch>
2025-05-26 16:07:04 +0200

    Export insertRecipe function

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,9 +2,15 @@ import { MongoClient } from 'mongodb';
 import { MONGODB_URI } from '$env/static/private';
 
 const client = new MongoClient(MONGODB_URI);
-const dbPromise = client.connect().then(() => client.db('rezepteapp'));
+const dbPromise = client.connect().then(() => client.db('rezeptapp'));
 
 export async function getAllRecipes() {
   const db = await dbPromise;
   return db.collection('recipes').find().toArray();
+}
+
+// Diese Funktion fehlt offenbar oder ist nicht exportiert:
+export async function insertRecipe(recipe: Record<string, any>) {
+  const db = await dbPromise;
+  return db.collection('recipes').insertOne(recipe);
 }
